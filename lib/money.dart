a0da683bdccca6686666bcccd24bc2b16d7c93cd
mahmoud_alhamaky <mahmoud.alhamaky@gmail.com>
2024-11-25 20:51:01 +0000

    add my code

---
+++ lib/money.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class money extends StatefulWidget {
+  const money({super.key});
+
+  @override
+  State<money> createState() => _moneyState();
+}
+
+class _moneyState extends State<money> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Money Tracking',
+            style: TextStyle(color: Colors.yellow, fontSize: 22)),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
